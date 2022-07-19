e69a6b9df5267480ba74ccd1adb689e4e2dc93e2
webdevifti <eftialam@hotmail.com>
2022-07-19 13:15:58 +0600

    hero form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
+import { Container, Row, Col } from 'reactstrap'
 import HeroSlider from '../components/UI/HeroSlider'
 import Helmet from '../components/helmet/Helmet'
+import FindCarForm from '../components/UI/FIndCarForm'
 
 const Home = () => {
   return (
     <Helmet title="Home">
       <section className="p-0 hero__slider-section">
         <HeroSlider />
+
+        <div className="hero__form">
+          <Container>
+            <Row className="form__row">
+              <Col lg="4" md="4">
+                <div className="find__cars-left">
+                  <h2>Find your best car here</h2>
+                </div>
+              </Col>
+              <Col lg="8" md="8" sm="12">
+                <FindCarForm />
+              </Col>
+            </Row>
+
+          </Container>
+        </div>
       </section>
     </Helmet>
   )
