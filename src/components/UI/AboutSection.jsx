fe8a0bfc64563787ddf1be2817d2e6ad0faca195
webdevifti <eftialam@hotmail.com>
2022-07-20 11:53:13 +0600

    about page done

--- src/components/UI/AboutSection.jsx
+++ src/components/UI/AboutSection.jsx
@@ -4,9 +4,9 @@ import { Container,Row,Col } from 'reactstrap'
 import aboutImg from '../../assets/all-images/cars-img/bmw-offer.png';
 
 
-const AboutSection = () => {
+const AboutSection = ({aboutClass}) => {
   return (
-    <section className='about__section'>
+    <section className='about__section' style={aboutClass == "aboutPage" ? {marginTop: "0px"} : {marginTop: "280px"}}>
         <Container>
             <Row>
                 <Col lg="6" md="6">
